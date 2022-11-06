dd0a8b05331043b77f82c2941eec01de2ae3a317
Ana Beatriz <norbertoana16@gmail.com>
2022-11-05 23:31:11 -0300

    Update ansiedade.c

--- Lista 1/ansiedade.c
+++ Lista 1/ansiedade.c
@@ -12,25 +12,30 @@ A saída é composta por uma única linha contendo um número inteiro informando
 triagem.
 */
 
-int main(int argc, char const *argv[])
+int main()
 {
     char formulario[10];
-    int triagem = 0;
-    int sim = 0;
-    int i, n;
+    int triagem = 0, sim = 0, form = 0;
 
-    while(scanf("%s", formulario) != EOF){
-        if(i == 10 && sim >= 2){
-            triagem = 1;           
-        }
-        if(strcmp(formulario, "sim") == 0){
+    while(scanf("%s", &formulario) != EOF){
+        form++;
+
+        if (strcmp(formulario, "sim") == 0){
             sim++;
-            i++;
-            
+        }
+        if (sim >=2 && form == 10){
+            form = 0;
+            sim = 0;
+            triagem++;
+        }
+        if (form == 10){
+            form = 0;
+            sim = 0;
         }
         
-    }          
-    printf("%i\n", triagem);
-    return 0;
+    }
+    printf("%i\n", triagem);  
+    return 0;      
 }
+
 
