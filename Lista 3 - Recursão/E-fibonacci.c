2066ecbf17b4d9828a9782b494e241265a042ce5
Ana Beatriz <norbertoana16@gmail.com>
2022-12-03 21:00:26 -0300

    Lista 3 finalizada (fibonacci) ✅

---
+++ Lista 3 - Recursão/E-fibonacci.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+
+long int fibonacci(int pos) {
+
+    if(pos == 1 || pos == 2) return 1;
+    
+    static int l = 3;
+    static long int x = 1, y = 1, z;
+    
+    if(l <= pos)
+    {
+        z = x + y;
+        x = y;
+        y = z;
+        l++;
+        fibonacci(pos);
+    }
+
+    return z;
+}
+
+
+// int main(void)
+// {
+//     int n;
+//     long int res;
+    
+//     scanf("%d", &n);
+
+//     res = fibonacci(n);
+//     printf("%ld\n", res);
+// }
+
+
